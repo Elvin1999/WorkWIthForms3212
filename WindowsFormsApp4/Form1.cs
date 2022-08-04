8f97d26f445295c47f2ef02e7a149994dcea4cf2
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-08-04 10:30:51 +0400

    Add project files.

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public EventHandler<EventArgs> handler;
+        public Form1()
+        {
+            InitializeComponent();
+            handler = new EventHandler<EventArgs>(button5_Click);
+                
+
+        }
+        public List<string> Users { get; set; } = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 form2 = new Form2(textBox1.Text);
+            //form2.ShowDialog();
+
+
+
+            //Form2 form2=new Form2();
+            //form2.Username = textBox1.Text;
+            //form2.ShowDialog();
+
+
+
+            //Form2 form2 = new Form2();
+            //form2.Users = Users;
+
+            //this.Hide();
+
+            //if (form2.ShowDialog() == DialogResult.Cancel)
+            //{
+            //    this.Show();
+            //}
+
+
+
+            //Parent Child melumat oturmenin yollari
+            // 1.  By constructor
+            // 2. By Property 
+            //3.  By ShowDialog Overloading
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Users.Add(textBox1.Text);
+            textBox1.Text = String.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            user.Name = nameTxtb.Text;
+            user.Surname = surnameTxtb.Text;
+            user.Age = int.Parse(ageMTxtb.Text);
+
+            //Form2 form2 = new Form2();
+           // form2.ShowDialog(user);
+        }
+
+            //Form2 form2 = new Form2();
+        Form2 form2 = new Form2();
+        private void button4_Click(object sender, EventArgs e)
+        {
+            form2.Handler = handler;
+            form2.Show();
+
+        }
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = form2.Data;
+        }
+    }
+}
---
+++ WindowsFormsApp4/Form2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form2 : Form
+    {
+        //public Form2(string text)
+        //{
+        //    InitializeComponent();
+        //    label1.Text = text;
+        //}
+        //public string Username { get; set; }
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; label1.Text = username; }
+        }
+        private List<string> users;
+
+        public List<string> Users
+        {
+            get { return users; }
+            set { users = value; SetLabel(users); }
+        }
+
+        void SetLabel(List<string> users)
+        {
+            foreach (var item in users)
+            {
+                label1.Text += item + "\n";
+            }
+        }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = Username;
+        }
+
+        public DialogResult ShowDialog(User user)
+        {
+            label1.Text = $"Name : {user.Name}  Surname : {user.Surname}  Age : {user.Age}";
+            return ShowDialog();
+        }
+        public EventHandler<EventArgs> Handler { get; set; }
+        public string Data { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Data = textBox1.Text;
+            Handler.Invoke(sender,e);
+            //this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Data = textBox1.Text;
+            Handler.Invoke(sender, e);
+        }
+    }
+}
